d93d2f3d6092de75af4ac4413faa5e673e9f88ef
wanghong <1198394709@qq.com>
2018-12-13 17:50:22 +0800

    Hystrix DashBoard and Hystrix Turbine

--- service-panda/src/main/java/com/hom/servicepanda/ServicePandaApplication.java
+++ service-panda/src/main/java/com/hom/servicepanda/ServicePandaApplication.java
@@ -1,13 +1,18 @@
 package com.hom.servicepanda;
 
 import brave.sampler.Sampler;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +22,9 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableTurbine
 public class ServicePandaApplication {
 
 	public static void main(String[] args) {
@@ -37,9 +45,14 @@ public class ServicePandaApplication {
 	String port;
 
 	@RequestMapping("/panda")
+    @HystrixCommand(fallbackMethod = "pandaError")
 	public String home(@RequestParam(value = "name", defaultValue = "hom") String name) {
 		return "hi " + name + " ,i am from port:" + port;
 	}
+
+	public String pandaError(String name){
+	    return "the panda "+name+" has error";
+    }
 
 	@RequestMapping("/call")
 	public String callHome(){
